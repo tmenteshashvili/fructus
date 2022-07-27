2de18894ce7848ee4b8772dff706c93ce8d147e9
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-07-27 14:03:04 +0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Tako Menteshashvili on 27.07.22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
